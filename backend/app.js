53b21b3c405b4888419d6ff2e6f6133498e78130
Sukanna <roysukanna@gmail.com>
2020-12-30 01:58:44 +0530

    Fetching the hard coded posts

--- backend/app.js
+++ backend/app.js
@@ -8,11 +8,28 @@ const app= express();
 //   next();
 // });
 
-app.use('\api\posts' ,  (req , res, next) =>
+// app.use((req , res, next) =>
+// {
+//   res.send ('Hello from expr');
+// });
+
+app.use((req ,res , next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Header", "Origin, X-Requested-With,Content-Type,Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,PUT,OPTIONS");
+  next();
+});
+
+app.use('/api/posts' ,  (req , res, next) =>
 {
-  //res.send ('Hello from express');
-  const posts = [];
-  res.json();
+  const posts = [
+    {id : '1' , title : 'First Server side post' , content : 'This is coming from server'},
+    {id : '2' , title : 'Second Server side post' , content : 'This is also coming from server'},
+  ];
+  res.status(200).json({
+    message : 'Post fetched successfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
